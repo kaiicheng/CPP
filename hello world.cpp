317eaf2c615b67892b5badb76324be35d75d52e4
[your <tw841117@gmail.com>
2024-01-28 22:52:38 -0500

    upload hello world.cpp

---
+++ hello world.cpp
@@ -0,0 +1,10 @@
+// g++ "hello world.cpp" -o "hello world"
+// .\hello world.exe
+
+#include<iostream>
+using namespace std;
+int main(){
+    // std::cout << "Hello World!" << std::endl;
+    cout << "Hello World!";
+    return 0;
+}
